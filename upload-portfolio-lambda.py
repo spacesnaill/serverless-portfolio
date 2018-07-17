e3ff39f12017c54cf31eb010185a91a580574fd7
Patrick Geiger <patrickjgeiger@gmail.com>
2018-07-17 15:03:21 -0400

    added SNS notifications for if the build deployed successfully or not

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,18 +4,28 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3')
-    portfolio_bucket = s3.Bucket('test-site.patrickjgeiger.com')
-    build_bucket = s3.Bucket('portfolio-build.patrickjgeiger.com')
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:111848558579:deployPortfolioTopic')
     
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    try:
+        s3 = boto3.resource('s3')
+        portfolio_bucket = s3.Bucket('test-site.patrickjgeiger.com')
+        build_bucket = s3.Bucket('portfolio-build.patrickjgeiger.com')
+        
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm, 
+                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        print "Job done"
+        
+        topic.publish(Subject="Portfolio Deployed", Message="Portfolio Deployed Successfully")
+    except:
+        topic.publish(Subject="Portfolio Deploy Failed", Message="Portfolio was not successfully deployed")
+        raise
     
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm, 
-            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
     return 'Hello from Lambda'
-
